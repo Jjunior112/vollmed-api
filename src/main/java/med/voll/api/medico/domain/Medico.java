e416b710588d259a7c814c1efcd25dabccbd9586
jjunior112 <juarezgcjr@gmail.com>
2025-07-11 12:39:46 -0300

    feat: alteração da entidade Medico para incluir telefone

--- src/main/java/med/voll/api/medico/domain/Medico.java
+++ src/main/java/med/voll/api/medico/domain/Medico.java
@@ -2,12 +2,12 @@ package med.voll.api.medico.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
-import med.voll.api.endereco.DadosEndereco;
 import med.voll.api.endereco.domain.Endereco;
+import med.voll.api.medico.dto.DadosCadastroMedico;
 import med.voll.api.medico.enums.Especialidade;
 
 
-@Entity(name="medicos")
+@Entity
 @Table(name="medico")
 @Getter
 @Setter
@@ -20,10 +20,16 @@ public class Medico {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
 
+    @Column(unique = true)
     private String email;
 
+
+    private String telefone;
+
+    @Column(unique = true)
     private String crm;
 
     @Enumerated(EnumType.STRING)
@@ -31,4 +37,14 @@ public class Medico {
 
     @Embedded
     private Endereco endereco;
+
+    public Medico(DadosCadastroMedico data)
+    {
+        this.nome = data.nome();
+        this.email = data.email();
+        this.telefone = data.telefone();
+        this.crm = data.crm();
+        this.especialidade = data.especialidade();
+        this.endereco = new Endereco(data.endereco());
+    }
 }
