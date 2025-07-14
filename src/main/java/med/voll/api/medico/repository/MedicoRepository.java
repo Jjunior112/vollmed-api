2b0f273d9f8b0f0a3d2df5ac33226732c7ae1480
jjunior112 <juarezgcjr@gmail.com>
2025-07-14 01:51:04 -0300

    feat: adição de metodo para filtragem por medicos ativos

--- src/main/java/med/voll/api/medico/repository/MedicoRepository.java
+++ src/main/java/med/voll/api/medico/repository/MedicoRepository.java
@@ -1,7 +1,15 @@
 package med.voll.api.medico.repository;
 
 import med.voll.api.medico.domain.Medico;
+import med.voll.api.medico.dto.UpdateMedico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MedicoRepository extends JpaRepository<Medico,Long> {
+
+
+    Page<Medico> findAllByIsActiveTrue(Pageable paginacao);
 }
