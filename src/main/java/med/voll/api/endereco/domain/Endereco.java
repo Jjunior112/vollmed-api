51f8510f740a951fe02acb4b949ecc2c48102c40
jjunior112 <juarezgcjr@gmail.com>
2025-07-14 01:53:56 -0300

    feat: adição do metodo atualizar informações na entidade Endereco

--- src/main/java/med/voll/api/endereco/domain/Endereco.java
+++ src/main/java/med/voll/api/endereco/domain/Endereco.java
@@ -19,13 +19,39 @@ public class Endereco {
     private String complemento;
     private String numero;
 
-    public Endereco(DadosEndereco data) {
-        this.logradouro = data.logradouro();
-        this.bairro = data.bairro();
-        this.numero = data.numero();
-        this.complemento = data.complemento();
-        this.cidade = data.cidade();
-        this.cep = data.cep();
-        this.uf = data.uf();
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+        this.cidade = dados.cidade();
+        this.cep = dados.cep();
+        this.uf = dados.uf();
+    }
+
+    public void atualizarInformacoes(DadosEndereco dados) {
+
+        if (dados.logradouro() != null) {
+            this.logradouro = dados.logradouro();
+        }
+        if (dados.bairro() != null) {
+            this.bairro = dados.bairro();
+        }
+        if (dados.numero() != null) {
+            this.numero = dados.numero();
+        }
+        if (dados.complemento() != null) {
+            this.complemento = dados.complemento();
+        }
+        if (dados.cidade() != null) {
+            this.cidade = dados.cidade();
+        }
+        if (dados.cep() != null) {
+            this.cep = dados.cep();
+        }
+        if (dados.uf() != null) {
+            this.uf = dados.uf();
+        }
+
     }
 }
