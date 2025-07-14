b120545ea89206e4f3a8a5d33b0fe7177e940a78
jjunior112 <juarezgcjr@gmail.com>
2025-07-14 01:54:17 -0300

    feat: atualização do Dto de listagem para conter id do medico

--- src/main/java/med/voll/api/medico/dto/DadosListagemMedico.java
+++ src/main/java/med/voll/api/medico/dto/DadosListagemMedico.java
@@ -3,9 +3,9 @@ package med.voll.api.medico.dto;
 import med.voll.api.medico.domain.Medico;
 import med.voll.api.medico.enums.Especialidade;
 
-public record DadosListagemMedico(String nome, String email, String crm, Especialidade especialidade) {
+public record DadosListagemMedico(long id, String nome, String email, String crm, Especialidade especialidade, boolean isActive) {
     public DadosListagemMedico(Medico medico)
     {
-        this(medico.getNome(),medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(), medico.getNome(),medico.getEmail(), medico.getCrm(), medico.getEspecialidade(), medico.isActive());
     }
 }
