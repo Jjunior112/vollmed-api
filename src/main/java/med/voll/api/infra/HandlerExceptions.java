e03a128e789607357482a2534d2340b7d05c1f8b
jjunior112 <juarezgcjr@gmail.com>
2025-07-14 16:50:17 -0300

    feat: criação da classe de handler de erros

---
+++ src/main/java/med/voll/api/infra/HandlerExceptions.java
@@ -0,0 +1,33 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+
+public class HandlerExceptions extends RuntimeException {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity error404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+
+    public ResponseEntity error400(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(errors.stream().map(FieldsErrors::new).toList());
+    }
+
+    private record FieldsErrors(String field, String message){
+        public FieldsErrors(FieldError error)
+        {
+            this(error.getField(),error.getDefaultMessage());
+        }
+    }
+}
