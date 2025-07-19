ee63814af03e6cc20433dc9c94f323a6a3034081
jjunior112 <juarezgcjr@gmail.com>
2025-07-18 21:59:57 -0300

    feat: create de service para consultas

---
+++ src/main/java/med/voll/api/application/services/ConsultaService.java
@@ -0,0 +1,86 @@
+package med.voll.api.application.services;
+
+import jakarta.validation.ValidationException;
+import med.voll.api.domain.models.Consulta;
+import med.voll.api.domain.models.Medico;
+import med.voll.api.domain.models.Paciente;
+import med.voll.api.domain.dtos.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.dtos.consulta.DadosDetalhamentoConsulta;
+import med.voll.api.infra.repositories.ConsultaRepository;
+import med.voll.api.infra.repositories.MedicoRepository;
+import med.voll.api.infra.repositories.PacienteRepository;
+import med.voll.api.infra.validations.Validation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ConsultaService {
+
+    @Autowired
+    private ConsultaRepository repository;
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+    @Autowired
+    private List<Validation> validators;
+
+    public Consulta agendarConsulta(DadosAgendamentoConsulta dadosConsulta) {
+
+        if (!pacienteRepository.existsById(dadosConsulta.idPaciente())) {
+            throw new ValidationException("Paciente informado não existe");
+        }
+
+        validators.forEach(v -> v.validate(dadosConsulta));
+
+        Medico medico = escolherMedico(dadosConsulta);
+
+        Paciente paciente = pacienteRepository.getReferenceById(dadosConsulta.idPaciente());
+
+        Consulta consulta = new Consulta(medico, paciente, dadosConsulta.data());
+
+        repository.save(consulta);
+
+        return consulta;
+
+    }
+
+    public Page<DadosDetalhamentoConsulta> findAll(Pageable paginacao) {
+
+        return repository.findAll(paginacao).map(DadosDetalhamentoConsulta::new);
+
+    }
+
+    public DadosDetalhamentoConsulta findConsultaById(Long id) {
+        Consulta consulta = repository.getReferenceById(id);
+
+        return new DadosDetalhamentoConsulta(consulta);
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dadosConsulta) {
+
+
+        if (dadosConsulta.idMedico() != null) {
+            return medicoRepository.getReferenceById(dadosConsulta.idMedico());
+        }
+
+        if (dadosConsulta.especialidade() == null) {
+            throw new ValidationException("Especialidade é obrigatória quando médico não for escolhido");
+        }
+
+        Medico medico = medicoRepository.MedicoAleatorio(dadosConsulta.especialidade(), dadosConsulta.data());
+
+        if (medico == null) {
+            throw new ValidationException("Especialidade não disponível nessa data");
+        }
+
+        return medico;
+    }
+
+}
