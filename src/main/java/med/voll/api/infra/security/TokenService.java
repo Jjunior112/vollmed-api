f9df5a62c0cfdfc6bf9e509ec790d42e316f7020
jjunior112 <juarezgcjr@gmail.com>
2025-07-18 22:03:46 -0300

    feat: adição de metodo de verificar token JWT

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,28 +3,31 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
-import med.voll.api.domain.User;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import med.voll.api.domain.models.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-
 
 @Service
 public class TokenService {
 
     @Value("${api.security.token.secret}")
     private String secret;
+
+    @Value("${api.security.issuer}")
+    private String issuer;
+
     public String generateToken(User user) {
         try {
             var algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
-                    .withIssuer("api Voll.med")
+                    .withIssuer(issuer)
                     .withSubject(user.getLogin())
-                    .withExpiresAt(dataExpiracao())
+                    .withExpiresAt(expires())
                     .sign(algorithm);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar token jwt", exception);
@@ -32,7 +35,24 @@ public class TokenService {
 
     }
 
-    private Instant dataExpiracao() {
+    public String validateToken(String token) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+
+                    .withIssuer(issuer)
+                    .build()
+
+                    .verify(token)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token invalido ou expirado!");
+        }
+    }
+
+    private Instant expires() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 
